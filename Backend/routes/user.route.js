4258fcba59b33a1cad5a28b2b5ab2afb35a9aa63
Meet Vaghasiya <vaghasiyam910@gmail.com>
2025-03-01 12:04:18 +0530

    comi

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -5,11 +5,15 @@ const userController = require('../controllers/user.controller');
 const authMiddleware = require('../Middlewares/auth.middleware');
 
 // User registration route
-router.post('/register', [  
+router.post('/register', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First Name must be at least 3 characters long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-], userController.registerUser);
+  ], userController.registerUser(req, res, next);
+});
 
 // User login route
 router.post('/login', [
